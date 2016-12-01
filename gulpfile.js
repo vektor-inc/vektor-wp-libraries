f9fdfbe8f3de51ff5458024d6e5315aa9ab47908
kurudrive <kurudrive@gmail.com>
2016-12-01 14:36:04 +0900

    [ gulp ] header-topのcssも監視対象に / origin pro へのコピー追加など

--- gulpfile.js
+++ gulpfile.js
@@ -21,7 +21,8 @@ gulp.task( 'copy_vk-admin', function() {
 gulp.task( 'copy_term-color', function() {
     gulp.src( './term-color/package/**' )
     .pipe( gulp.dest( '../plugins/vk-post-author-display/inc/term-color/' ) )
-    .pipe( gulp.dest( '../plugins/lightning-media-unit/inc/term-color/' ) );
+    .pipe( gulp.dest( '../plugins/lightning-skin-variety/inc/term-color/' ) )
+    .pipe( gulp.dest( '../plugins/lightning-origin-pro/inc/term-color/' ) );
 } );
 gulp.task( 'copy_header-top', function() {
     gulp.src( './header-top/package/**' )
@@ -50,6 +51,7 @@ gulp.task('watch', function() {
     gulp.watch('./vk-admin/package/class.vk-admin.php', ['copy_vk-admin']);
     gulp.watch('./term-color/package/**', ['copy_term-color']);
     gulp.watch('./header-top/package/**', ['copy_header-top']);
+    gulp.watch('./header-top/package/css/**', ['copy_header-top']);
     gulp.watch('./media-posts/package/**', ['copy_media-posts']);
     gulp.watch('./post-type-manager/package/**', ['copy_post-type-manager']);
     // gulp.watch('./font-awesome-selector/package/**', ['copy_font-awesome-selector']);
