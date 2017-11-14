b2f70db8ee377d3a227eead539df90bbec761ac4
kurudrive <kurudrive@gmail.com>
2017-11-14 13:36:20 +0900

    [ cf builder ] 変数名が誤解しやすいので修正

--- custom-field-builder/custom-field-builder-config.php
+++ custom-field-builder/custom-field-builder-config.php
@@ -7,6 +7,6 @@ if ( ! class_exists( 'VK_Custom_Field_Builder' ) )
 {
 	require_once( 'custom-field-builder/custom-field-builder.php' );
 
-	global $custom_field_builder_dir;
-	$custom_field_builder_dir = LIGHTNING_SKIN_JPNSTYLE_URL.'inc/custom-field-builder/';
+	global $custom_field_builder_url;
+	$custom_field_builder_url = LIGHTNING_SKIN_JPNSTYLE_URL.'inc/custom-field-builder/';
 }
