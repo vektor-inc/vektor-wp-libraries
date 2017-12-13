a693ca77fad93075bc5371137e1915cebc09fdd1
catherine <catherine5th.avenue@gmail.com>
2017-12-13 11:13:13 +0900

    フィールド名変更（途中）

--- vk-widget-pr-content/package/class-vk-widget-pr-content.php
+++ vk-widget-pr-content/package/class-vk-widget-pr-content.php
@@ -32,10 +32,9 @@ class VK_Widget_Pr_Content extends WP_Widget {
   {
     $defaults = array(
       'title'       => '',
-      'pr_content_text'        => '',
-      'pr_content_media_image' => null,
-      'pr_content_media_alt'   => null,
-      // 'pr_content_label' => __( 'PR Content area1 title', $pr_content_textdomain ),
+      'text'        => '',
+      'media_image' => null,
+      'media_alt'   => null,
       'pr_content_btn_text'    => '',
       'pr_content_btn_url'     => '',
       'pr_content_btn_blank'   => false,
@@ -65,15 +64,15 @@ class VK_Widget_Pr_Content extends WP_Widget {
     <div class="col-sm-6 pr-content-col-left"><?php
     // media img
     // 画像IDから画像のURLを取得
-    if ( ! empty( $options['pr_content_media_image'] ) ) {
-      $image = wp_get_attachment_image_src( $options['pr_content_media_image'], 'large' );
+    if ( ! empty( $options['media_image'] ) ) {
+      $image = wp_get_attachment_image_src( $options['media_image'], 'large' );
       $image = $image[0];
     } else {
       $image = null;
     }
     // 画像が登録されている場合
     if ( ! empty( $image ) ) {
-      echo  '<img class="pr_content_media_imgage" src="'.esc_url( $image ).'" alt="'.esc_attr( $options['pr_content_media_alt'] ).'" style="border: 1px solid #ccc;" class="card-img-top" />';
+      echo  '<img class="pr_content_media_imgage" src="'.esc_url( $image ).'" alt="'.esc_attr( $options['media_alt'] ).'" style="border: 1px solid #ccc;" class="card-img-top" />';
     }
     ?></div><!-- .col-sm-6 -->
     <div class="col-sm-6 pr-content-col-right"><?php
@@ -82,7 +81,7 @@ class VK_Widget_Pr_Content extends WP_Widget {
       echo '<h3 class="pr-content-title">'.esc_html( $options['title'] ).'</h3>';
     }
     // text
-    echo '<p>'.wp_kses_post( $options['pr_content_text'] ).'</p>';
+    echo '<p>'.wp_kses_post( $options['text'] ).'</p>';
     // link btn
     if ( ! empty ( $options['pr_content_btn_text'] ) && ! empty ( $options['pr_content_btn_url'] )) {
       $more_link_html = '<div class="pr-content-btn">';
@@ -165,9 +164,9 @@ class VK_Widget_Pr_Content extends WP_Widget {
 	{
 		$instance = $old_instance;
 		$instance[ 'title' ] = wp_kses_post( $new_instance[ 'title' ] );
-		$instance[ 'pr_content_text' ] = wp_kses_post( $new_instance[ 'pr_content_text' ] );
-		$instance[ 'pr_content_media_image' ] = wp_kses_post( $new_instance[ 'pr_content_media_image' ] );
-		$instance[ 'pr_content_media_alt' ] = esc_attr( $new_instance[ 'pr_content_media_alt' ] );
+		$instance[ 'text' ] = wp_kses_post( $new_instance[ 'text' ] );
+		$instance[ 'media_image' ] = wp_kses_post( $new_instance[ 'media_image' ] );
+		$instance[ 'media_alt' ] = esc_attr( $new_instance[ 'media_alt' ] );
 		$instance[ 'pr_content_btn_text' ] = wp_kses_post( $new_instance[ 'pr_content_btn_text' ] );
 		$instance[ 'pr_content_btn_url' ] = esc_url( $new_instance[ 'pr_content_btn_url' ] );
 		$instance[ 'pr_content_btn_blank' ] = ( isset( $new_instance[ 'pr_content_btn_blank' ] ) && $new_instance[ 'pr_content_btn_blank' ] ) ? true : false;
@@ -187,15 +186,15 @@ class VK_Widget_Pr_Content extends WP_Widget {
       <label for="<?php echo $this->get_field_id('title'); ?>" ><?php _e('Title:', $pr_content_textdomain); ?></label>
       <input type="text" id="<?php echo $this->get_field_id('title'); ?>-title" name="<?php echo $this->get_field_name('title'); ?>" style="width:100%; margin-bottom: 1.5em;" value="<?php echo esc_attr( $options['title'] ); ?>"></input>
 
-      <label for="<?php echo $this->get_field_id('pr_content_text'); ?>" ><?php _e('Text:', $pr_content_textdomain); ?></label>
-      <textarea id="<?php echo $this->get_field_id('pr_content_text'); ?>" name="<?php echo $this->get_field_name('pr_content_text'); ?>" style="width:100%; margin-bottom: 1.5em;"><?php echo esc_textarea( $options['pr_content_text'] ); ?></textarea>
+      <label for="<?php echo $this->get_field_id('text'); ?>" ><?php _e('Text:', $pr_content_textdomain); ?></label>
+      <textarea id="<?php echo $this->get_field_id('text'); ?>" name="<?php echo $this->get_field_name('text'); ?>" style="width:100%; margin-bottom: 1.5em;"><?php echo esc_textarea( $options['text'] ); ?></textarea>
 
 			<?php
       $image = null;
       // ちゃんと数字が入っているかどうか？
-      if ( is_numeric( $options['pr_content_media_image'] ) ) {
+      if ( is_numeric( $options['media_image'] ) ) {
         // 数字だったら、その数字の画像を large サイズで取得
-          $image = wp_get_attachment_image_src( $options['pr_content_media_image'], 'large' );
+          $image = wp_get_attachment_image_src( $options['media_image'], 'large' );
       }
       ?>
       <div class="pr_content_media_area" style="padding: 0.5em 0 3.0em;">
@@ -205,16 +204,17 @@ class VK_Widget_Pr_Content extends WP_Widget {
           <?php endif; ?>
 
       </div>
-      <button class="button button-default button-block" style="display:block;width:100%;text-align: center; margin:0;" onclick="javascript:pr_content_media_image_addiditional(this);return false;"><?php _e('Set image', $pr_content_textdomain ); ?></button>
+      <button class="button button-default button-block" style="display:block;width:100%;text-align: center; margin:0;" onclick="javascript:media_image_addiditional(this);return false;"><?php _e('Set image', $pr_content_textdomain ); ?></button>
       <button class="button button-default button-block" style="display:block;width:100%;text-align: center; margin:4px 0;" onclick="javascript:vk_title_bg_image_delete(this);return false;"><?php _e('Delete image', $pr_content_textdomain ); ?></button>
       <div class="_form" style="line-height: 2em">
-          <input type="hidden" class="__id" name="<?php echo $this->get_field_name( 'pr_content_media_image' ); ?>" value="<?php echo esc_attr( $options['pr_content_media_image'] ); ?>" />
+          <input type="hidden" class="__id" name="<?php echo $this->get_field_name( 'media_image' ); ?>" value="<?php echo esc_attr( $options['media_image'] ); ?>" />
+          <input type="hidden" class="_alt" name="<?php echo $this->get_field_name( 'media_alt' ); ?>" value="<?php echo esc_attr( $options['media_alt'] ); ?>" />
       </div>
       </div>
 			<script type="text/javascript">
       // 画像登録処理
-      if ( pr_content_media_image_addiditional == undefined ){
-      var pr_content_media_image_addiditional = function(e){
+      if ( media_image_addiditional == undefined ){
+      var media_image_addiditional = function(e){
       		// プレビュー画像を表示するdiv
           var d=jQuery(e).parent().children("._display");
       		// 画像IDを保存するinputタグ
@@ -230,8 +230,8 @@ class VK_Widget_Pr_Content extends WP_Widget {
       };
       }
       // 背景画像削除処理
-      if ( vk_title_bg_image_delete == undefined ){
-      var vk_title_bg_image_delete = function(e){
+      if ( media_image_delete == undefined ){
+      var media_image_delete = function(e){
       		// プレビュー画像を表示するdiv
       		var d=jQuery(e).parent().children("._display");
       		// 画像IDを保存するinputタグ
