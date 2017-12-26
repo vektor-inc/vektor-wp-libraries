9433d2481e02847da79d54356be6c7a499615179
kurudrive <kurudrive@gmail.com>
2017-12-26 10:55:49 +0900

    [ post type manager ] 整形のみ

--- post-type-manager/package/class.post-type-manager.php
+++ post-type-manager/package/class.post-type-manager.php
@@ -22,11 +22,11 @@ if ( ! class_exists( 'Vk_post_type_manager' ) ) {
 						'singular_name' => __( 'Custom Post Type Setting', $vk_post_type_manager_textdomain ),
 					),
 					'public' => false,
-			        'show_ui' => true,
-			        'show_in_menu' => true,
+					'show_ui' => true,
+					'show_in_menu' => true,
 					'menu_position' => 100,
-			        'capability_type' => array('post_type_manage','post_type_manages'),
-			        'map_meta_cap' => true,
+					'capability_type' => array('post_type_manage','post_type_manages'),
+					'map_meta_cap' => true,
 					'has_archive' => false,
 					'menu_icon' => 'dashicons-admin-generic',
 					'supports' => array('title')
@@ -34,38 +34,38 @@ if ( ! class_exists( 'Vk_post_type_manager' ) ) {
 			);
 		}
 
-        /*-------------------------------------------*/
-        /*  post_type_manage の編集権限を追加
-        /*-------------------------------------------*/
-        function add_cap_post_type_manage() {
-        	$role = get_role('administrator');
-			$post_type_name = 'post_type_manage';
-			$role->add_cap( 'add_'.$post_type_name );
-			$role->add_cap( 'add_'.$post_type_name.'s' );
-			$role->add_cap( 'edit_'.$post_type_name );
-			$role->add_cap( 'edit_'.$post_type_name.'s' );
-			$role->add_cap( 'edit_published_'.$post_type_name.'s' );
-			$role->add_cap( 'edit_others_'.$post_type_name.'s' );
-			$role->add_cap( 'delete_'.$post_type_name );
-			$role->add_cap( 'delete_'.$post_type_name.'s' );
-			$role->add_cap( 'delete_private_'.$post_type_name.'s' );
-			$role->add_cap( 'delete_others_'.$post_type_name.'s' );
-			$role->add_cap( 'delete_published_'.$post_type_name.'s' );
-			$role->add_cap( 'publish_'.$post_type_name.'s' );
-			$role->add_cap( 'publish_others_'.$post_type_name.'s' );
-        }
+			/*-------------------------------------------*/
+			/*  post_type_manage の編集権限を追加
+			/*-------------------------------------------*/
+			function add_cap_post_type_manage() {
+				$role = get_role('administrator');
+				$post_type_name = 'post_type_manage';
+				$role->add_cap( 'add_'.$post_type_name );
+				$role->add_cap( 'add_'.$post_type_name.'s' );
+				$role->add_cap( 'edit_'.$post_type_name );
+				$role->add_cap( 'edit_'.$post_type_name.'s' );
+				$role->add_cap( 'edit_published_'.$post_type_name.'s' );
+				$role->add_cap( 'edit_others_'.$post_type_name.'s' );
+				$role->add_cap( 'delete_'.$post_type_name );
+				$role->add_cap( 'delete_'.$post_type_name.'s' );
+				$role->add_cap( 'delete_private_'.$post_type_name.'s' );
+				$role->add_cap( 'delete_others_'.$post_type_name.'s' );
+				$role->add_cap( 'delete_published_'.$post_type_name.'s' );
+				$role->add_cap( 'publish_'.$post_type_name.'s' );
+				$role->add_cap( 'publish_others_'.$post_type_name.'s' );
+			}
 
-        /*-------------------------------------------*/
-        /*  meta box を作成
-        /*-------------------------------------------*/
-        // add meta_box
-        function add_meta_box() {
-        	global $vk_post_type_manager_textdomain;
-            add_meta_box( 'meta_box_post_type_manage', __('Custom Post Type Setting',$vk_post_type_manager_textdomain), array( $this, 'add_meta_box_action' ), 'post_type_manage','normal','high' );
-        }
-        function add_meta_box_action(){
-        	global $vk_post_type_manager_textdomain;
-            global $post;
+			/*-------------------------------------------*/
+			/*  meta box を作成
+			/*-------------------------------------------*/
+			// add meta_box
+			function add_meta_box() {
+				global $vk_post_type_manager_textdomain;
+				add_meta_box( 'meta_box_post_type_manage', __('Custom Post Type Setting',$vk_post_type_manager_textdomain), array( $this, 'add_meta_box_action' ), 'post_type_manage','normal','high' );
+			}
+			function add_meta_box_action(){
+				global $vk_post_type_manager_textdomain;
+				global $post;
 
 		    //CSRF対策の設定（フォームにhiddenフィールドとして追加するためのnonceを「'noncename__post_type_manager」として設定）
 		    wp_nonce_field( wp_create_nonce(__FILE__), 'noncename__post_type_manager' );
@@ -82,40 +82,43 @@ if ( ! class_exists( 'Vk_post_type_manager' ) ) {
 		    </style>
 		    <?php
 
-		    // Post Type ID
-            echo '<h4>'.__('Post Type ID(Required)', $vk_post_type_manager_textdomain).'</h4>';
-            echo '<p>'.__( '20 characters or less in alphanumeric',$vk_post_type_manager_textdomain).'</p>';
-            echo '<input class="form-control" type="text" id="veu_post_type_id" name="veu_post_type_id" value="'.esc_attr($post->veu_post_type_id).'" size="30">';
-            echo '<hr>';
+				/* Post Type ID
+				/*-------------------------------------------*/
+				echo '<h4>'.__('Post Type ID(Required)', $vk_post_type_manager_textdomain).'</h4>';
+				echo '<p>'.__( '20 characters or less in alphanumeric',$vk_post_type_manager_textdomain).'</p>';
+				echo '<input class="form-control" type="text" id="veu_post_type_id" name="veu_post_type_id" value="'.esc_attr($post->veu_post_type_id).'" size="30">';
+				echo '<hr>';
 
-            $post_type_items_array = array(
-            	'title'     => __( 'title', $vk_post_type_manager_textdomain ),
-				'editor'    => __( 'editor', $vk_post_type_manager_textdomain ),
-				'author'    => __( 'author', $vk_post_type_manager_textdomain ),
-				'thumbnail' => __( 'thumbnail', $vk_post_type_manager_textdomain ),
-				'excerpt'   => __( 'excerpt', $vk_post_type_manager_textdomain ),
-				'comments'  => __( 'comments', $vk_post_type_manager_textdomain ),
-				'revisions' => __( 'revisions', $vk_post_type_manager_textdomain ),
+				$post_type_items_array = array(
+					'title'     => __( 'title', $vk_post_type_manager_textdomain ),
+					'editor'    => __( 'editor', $vk_post_type_manager_textdomain ),
+					'author'    => __( 'author', $vk_post_type_manager_textdomain ),
+					'thumbnail' => __( 'thumbnail', $vk_post_type_manager_textdomain ),
+					'excerpt'   => __( 'excerpt', $vk_post_type_manager_textdomain ),
+					'comments'  => __( 'comments', $vk_post_type_manager_textdomain ),
+					'revisions' => __( 'revisions', $vk_post_type_manager_textdomain ),
 				);
 
-            // Post Type ID
-		    echo '<h4>'.__('Supports(Required)', $vk_post_type_manager_textdomain ).'</h4>';
-		    $post_type_items_value = get_post_meta( $post->ID,'veu_post_type_items',true );
-		    echo '<ul>';
-		    foreach ($post_type_items_array as $key => $label) {
-		    	$checked = ( isset( $post_type_items_value[$key] ) && $post_type_items_value[$key] ) ? ' checked':'';
-			    echo '<li><label>'.'<input type="checkbox" id="veu_post_type_items['.$key.']" name="veu_post_type_items['.$key.']" value="true"'.$checked.'> '.$label.'</label></li>';
-		    }
-		    echo '</ul>';
+				/* Supports(Required)
+				/*-------------------------------------------*/
+				echo '<h4>'.__('Supports(Required)', $vk_post_type_manager_textdomain ).'</h4>';
+				$post_type_items_value = get_post_meta( $post->ID,'veu_post_type_items',true );
+				echo '<ul>';
+				foreach ($post_type_items_array as $key => $label) {
+					$checked = ( isset( $post_type_items_value[$key] ) && $post_type_items_value[$key] ) ? ' checked':'';
+					echo '<li><label>'.'<input type="checkbox" id="veu_post_type_items['.$key.']" name="veu_post_type_items['.$key.']" value="true"'.$checked.'> '.$label.'</label></li>';
+				}
+				echo '</ul>';
 
-		    echo '<hr>';
+				echo '<hr>';
 
-		    // Menu position
-		   	echo '<h4>'.__('Menu position(optional)', $vk_post_type_manager_textdomain ).'</h4>';
-		   	echo '<p>'.__('Please enter a number.', $vk_post_type_manager_textdomain ).'</p>';
-            echo '<input class="form-control" type="text" id="veu_menu_position" name="veu_menu_position" value="'.esc_attr($post->veu_menu_position).'" size="30">';
+				/* Menu position
+				/*-------------------------------------------*/
+				echo '<h4>'.__('Menu position(optional)', $vk_post_type_manager_textdomain ).'</h4>';
+				echo '<p>'.__('Please enter a number.', $vk_post_type_manager_textdomain ).'</p>';
+				echo '<input class="form-control" type="text" id="veu_menu_position" name="veu_menu_position" value="'.esc_attr($post->veu_menu_position).'" size="30">';
 
-            echo '<hr>';
+				echo '<hr>';
 
 				/* Export to Rest api
 				/*-------------------------------------------*/
@@ -181,7 +184,7 @@ if ( ! class_exists( 'Vk_post_type_manager' ) ) {
 				array ( 'category' => 'カテゴリー' ),
 			 );
 
-        }
+		}
 
 		/*-------------------------------------------*/
 		/*  入力された値の保存
@@ -211,17 +214,20 @@ if ( ! class_exists( 'Vk_post_type_manager' ) ) {
 				);
 
 		    foreach ($fields as $key => $field) {
-			    $field_value = ( isset( $_POST[$field] ) ) ? $_POST[$field] : '';
-			    // データが空だったら入れる
-			    if( get_post_meta($post_id, $field ) == ""){
-			        add_post_meta($post_id, $field , $field_value, true);
-			    // 今入ってる値と違ってたらアップデートする
-			    } elseif( $field_value != get_post_meta( $post_id, $field , true)){
-			        update_post_meta($post_id, $field , $field_value);
-			    // 入力がなかったら消す
-			    } elseif( $field_value == "" ){
-			        delete_post_meta($post_id, $field , get_post_meta( $post_id, $field , true ));
-			    }
+					$field_value = ( isset( $_POST[$field] ) ) ? $_POST[$field] : '';
+
+					// データが空だったら入れる
+					if( get_post_meta($post_id, $field ) == ""){
+						add_post_meta($post_id, $field , $field_value, true);
+
+					// 今入ってる値と違ってたらアップデートする
+					} elseif( $field_value != get_post_meta( $post_id, $field , true)){
+						update_post_meta($post_id, $field , $field_value);
+
+					// 入力がなかったら消す
+					} elseif( $field_value == "" ){
+						delete_post_meta($post_id, $field , get_post_meta( $post_id, $field , true ));
+					}
 		    }
 
 		}
@@ -310,35 +316,33 @@ if ( ! class_exists( 'Vk_post_type_manager' ) ) {
 											'show_ui' => true,
 										)
 									);
-							}
+							} // if ( $taxonomy['slug'] && $taxonomy['label']){
 
-						}
+						} // foreach ($veu_taxonomies as $key => $taxonomy) {
 
 					} // if ( $post_type_id ) {
 
-
-
 				} // foreach ($custom_post_types as $key => $post) {
 
-		    } // if ( $custom_post_types ) {
+			} // if ( $custom_post_types ) {
 
 		}
 
-        /*-------------------------------------------*/
-        /*  実行
-        /*-------------------------------------------*/
-        public function __construct(){
-            add_action( 'init', array( $this, 'add_post_type_post_type_manage' ) );
-            add_action( 'admin_init', array( $this, 'add_cap_post_type_manage' ) );
-            add_action( 'save_post', array( $this, 'save_cf_value') );
-            add_action( 'admin_menu', array( $this, 'add_meta_box' ) );
-            add_action( 'init', array( $this, 'add_post_type' ) ,0);
-            // add_action( 'save_post', array( $this, 'add_post_notice' ) );
-            add_action( 'admin_notices', array( $this, 'add_post_notice' ) );
-        }
+			/*-------------------------------------------*/
+			/*  実行
+			/*-------------------------------------------*/
+			public function __construct(){
+				add_action( 'init', array( $this, 'add_post_type_post_type_manage' ) );
+				add_action( 'admin_init', array( $this, 'add_cap_post_type_manage' ) );
+				add_action( 'save_post', array( $this, 'save_cf_value') );
+				add_action( 'admin_menu', array( $this, 'add_meta_box' ) );
+				add_action( 'init', array( $this, 'add_post_type' ) ,0);
+				// add_action( 'save_post', array( $this, 'add_post_notice' ) );
+				add_action( 'admin_notices', array( $this, 'add_post_notice' ) );
+			}
 
-    } // class Vk_post_type_manager
+		} // class Vk_post_type_manager
 
-    $Vk_post_type_manager = new Vk_post_type_manager();
+		$Vk_post_type_manager = new Vk_post_type_manager();
 
 }
