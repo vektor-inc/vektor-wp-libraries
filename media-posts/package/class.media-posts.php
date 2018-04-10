55626e05e324f9f0e42eb9c5c3b5b88cea6df0c7
kurudrive <kurudrive@gmail.com>
2018-04-10 10:01:13 +0900

    phpcs

--- media-posts/package/class.media-posts.php
+++ media-posts/package/class.media-posts.php
@@ -1,85 +1,84 @@
 <?php
-if ( ! class_exists( 'Lightning_media_posts' ) )
-{
-	class Lightning_media_posts
-	{
+if ( ! class_exists( 'Lightning_media_posts' ) ) {
+	class Lightning_media_posts {
 
-		static function init(){
+
+		static function init() {
 				define( 'LTG_MEDIA_POSTS_URL', plugin_dir_url( __FILE__ ) );
 				define( 'LTG_MEDIA_POSTS_DIR', plugin_dir_path( __FILE__ ) );
 				define( 'LTG_MEDIA_POSTS_VERSION', '1.1' );
 		}
 
-		public static function patterns(){
+		public static function patterns() {
 			global $vk_ltg_media_posts_textdomain;
 			$patterns = array(
-				'image_1st' => array(
-					'label' => __( 'Image card 1st feature', 'lightning-media-unit' ),
+				'image_1st'            => array(
+					'label'       => __( 'Image card 1st feature', 'lightning-media-unit' ),
 					'class_outer' => 'image_1st',
 					// 1stは２件目以降とpostのクラスが異なるためここに値が無い
-				 ),
-				'image_3' => array(
-					'label' => __( 'Image card 3 colmun', 'lightning-media-unit' ),
-					'class_outer' => 'row image_3',
+				),
+				'image_3'              => array(
+					'label'            => __( 'Image card 3 colmun', 'lightning-media-unit' ),
+					'class_outer'      => 'row image_3',
 					'class_post_outer' => 'col-sm-6 col-lg-4 col-md-4',
-					'class_post_item' => ' image_card',
-				 ),
-				'image_1' => array(
-					'label' => __( 'Image card 1 colmun', 'lightning-media-unit' ),
-					'class_outer' => 'row image_1',
+					'class_post_item'  => ' image_card',
+				),
+				'image_1'              => array(
+					'label'            => __( 'Image card 1 colmun', 'lightning-media-unit' ),
+					'class_outer'      => 'row image_1',
 					'class_post_outer' => 'col-sm-12',
-					'class_post_item' => ' image_card'
-				 ),
-				'vert_3' => array(
-					'label' => __( 'Vertical card 3 column', 'lightning-media-unit' ),
-					'class_outer' => 'row vert_3 flex_height',
+					'class_post_item'  => ' image_card',
+				),
+				'vert_3'               => array(
+					'label'            => __( 'Vertical card 3 column', 'lightning-media-unit' ),
+					'class_outer'      => 'row vert_3 flex_height',
 					'class_post_outer' => 'col-sm-6 col-lg-4 col-md-4 flex_height_col',
-					'class_post_item' => ' vertical_card normal_image'
-				 ),
-				'vert_1' => array(
-					'label' => __( 'Vertical card 1 column', 'lightning-media-unit' ),
-					'class_outer' => 'row vert_1',
+					'class_post_item'  => ' vertical_card normal_image',
+				),
+				'vert_1'               => array(
+					'label'            => __( 'Vertical card 1 column', 'lightning-media-unit' ),
+					'class_outer'      => 'row vert_1',
 					'class_post_outer' => 'col-sm-12',
-					'class_post_item' => ' vertical_card normal_image'
-				 ),
-				'vert_large_image_3' => array(
-					'label' => __( 'Vertical card large image 3 column', 'lightning-media-unit' ),
-					'class_outer' => 'row vert_large_image_3 flex_height',
+					'class_post_item'  => ' vertical_card normal_image',
+				),
+				'vert_large_image_3'   => array(
+					'label'            => __( 'Vertical card large image 3 column', 'lightning-media-unit' ),
+					'class_outer'      => 'row vert_large_image_3 flex_height',
 					'class_post_outer' => 'col-sm-6 col-lg-4 col-md-4 flex_height_col',
-					'class_post_item' => ' vertical_card large_image'
-				 ),
-				'vert_large_image_1' => array(
-					'label' => __( 'Vertical card large image 1 column', 'lightning-media-unit' ),
-					'class_outer' => 'row vert_large_image_1',
+					'class_post_item'  => ' vertical_card large_image',
+				),
+				'vert_large_image_1'   => array(
+					'label'            => __( 'Vertical card large image 1 column', 'lightning-media-unit' ),
+					'class_outer'      => 'row vert_large_image_1',
 					'class_post_outer' => 'col-sm-12',
-					'class_post_item' => ' vertical_card large_image'
-				 ),
-				'oblong_1' => array(
-					'label' => __( 'Oblong card 1 column', 'lightning-media-unit' ),
-					'class_outer' => 'row oblong_1',
+					'class_post_item'  => ' vertical_card large_image',
+				),
+				'oblong_1'             => array(
+					'label'            => __( 'Oblong card 1 column', 'lightning-media-unit' ),
+					'class_outer'      => 'row oblong_1',
 					'class_post_outer' => 'col-sm-12',
-					'class_post_item' => ' oblong_card normal_image'
-				 ),
+					'class_post_item'  => ' oblong_card normal_image',
+				),
 				'oblong_large_image_1' => array(
-					'label' => __( 'Oblong card large image 1 column', 'lightning-media-unit' ),
-					'class_outer' => 'row oblong_large_image_1',
+					'label'            => __( 'Oblong card large image 1 column', 'lightning-media-unit' ),
+					'class_outer'      => 'row oblong_large_image_1',
 					'class_post_outer' => 'col-sm-12',
-					'class_post_item' => ' oblong_card large_image'
-				 ),
-				);
+					'class_post_item'  => ' oblong_card large_image',
+				),
+			);
 			return $patterns;
 		}
 
-		public static function patterns_select_options( $selected ){
+		public static function patterns_select_options( $selected ) {
 			global $vk_ltg_media_posts_textdomain;
 			$patterns = Lightning_media_posts::patterns();
 
-			$selected_html =  ( isset( $selected ) && ( $selected == 'default' ) ) ? ' selected' : '';
-			$select_html = '<option value="default">'.__('Lightning default', $vk_ltg_media_posts_textdomain).'</option>';
+			$selected_html = ( isset( $selected ) && ( $selected == 'default' ) ) ? ' selected' : '';
+			$select_html   = '<option value="default">' . __( 'Lightning default', $vk_ltg_media_posts_textdomain ) . '</option>';
 
 			foreach ( $patterns as $key => $value ) {
-				$selected_html =  ( isset( $selected ) && ( $selected == $key ) ) ? ' selected' : '';
-				$select_html .= '<option value="'.$key.'"'.$selected_html.'>'.$value['label'].'</option>'."\n";
+				$selected_html = ( isset( $selected ) && ( $selected == $key ) ) ? ' selected' : '';
+				$select_html  .= '<option value="' . $key . '"' . $selected_html . '>' . $value['label'] . '</option>' . "\n";
 			}
 			echo $select_html;
 		}
@@ -89,7 +88,7 @@ if ( ! class_exists( 'Lightning_media_posts' ) )
 
 			$post_types_labels = array(
 				'post' => __( 'Posts' ),
-				'page' => __( 'Pages' , 'lightning-media-unit' ),
+				'page' => __( 'Pages', 'lightning-media-unit' ),
 			);
 
 			return $post_types_labels;
@@ -120,7 +119,7 @@ if ( ! class_exists( 'Lightning_media_posts' ) )
 				'_builtin' => false,
 			);
 
-			$custom_types = get_post_types( $args, 'objects' );
+			$custom_types        = get_post_types( $args, 'objects' );
 			$custom_types_labels = array();
 
 			foreach ( $custom_types as $custom_type ) {
@@ -133,36 +132,41 @@ if ( ! class_exists( 'Lightning_media_posts' ) )
 		/*-------------------------------------------*/
 		/*  Widgets init
 		/*-------------------------------------------*/
-		static function widgets_init(){
+		static function widgets_init() {
 			global $vk_ltg_media_posts_textdomain;
-			register_sidebar( array(
-				'name' => __( 'Home content top after left', $vk_ltg_media_posts_textdomain ),
-				'id' => 'home-content-top-after-left-widget-area',
-				'before_widget' => '<div class="widget %2$s" id="%1$s">',
-				'after_widget' => '</div>',
-				'before_title' => '<h2 class="mainSection-title">',
-				'after_title' => '</h2>',
-			) );
-			register_sidebar( array(
-				'name' => __( 'Home content top after right', $vk_ltg_media_posts_textdomain ),
-				'id' => 'home-content-top-after-right-widget-area',
-				'before_widget' => '<div class="widget %2$s" id="%1$s">',
-				'after_widget' => '</div>',
-				'before_title' => '<h2 class="mainSection-title">',
-				'after_title' => '</h2>',
-			) );
-			register_sidebar( array(
-				'name' => __( 'Home content top bottom widget', $vk_ltg_media_posts_textdomain ),
-				'id' => 'home-content-top-bottom-widget-area',
-				'before_widget' => '<div class="widget %2$s" id="%1$s">',
-				'after_widget' => '</div>',
-				'before_title' => '<h2 class="mainSection-title">',
-				'after_title' => '</h2>',
-			) );
+			register_sidebar(
+				array(
+					'name'          => __( 'Home content top after left', $vk_ltg_media_posts_textdomain ),
+					'id'            => 'home-content-top-after-left-widget-area',
+					'before_widget' => '<div class="widget %2$s" id="%1$s">',
+					'after_widget'  => '</div>',
+					'before_title'  => '<h2 class="mainSection-title">',
+					'after_title'   => '</h2>',
+				)
+			);
+			register_sidebar(
+				array(
+					'name'          => __( 'Home content top after right', $vk_ltg_media_posts_textdomain ),
+					'id'            => 'home-content-top-after-right-widget-area',
+					'before_widget' => '<div class="widget %2$s" id="%1$s">',
+					'after_widget'  => '</div>',
+					'before_title'  => '<h2 class="mainSection-title">',
+					'after_title'   => '</h2>',
+				)
+			);
+			register_sidebar(
+				array(
+					'name'          => __( 'Home content top bottom widget', $vk_ltg_media_posts_textdomain ),
+					'id'            => 'home-content-top-bottom-widget-area',
+					'before_widget' => '<div class="widget %2$s" id="%1$s">',
+					'after_widget'  => '</div>',
+					'before_title'  => '<h2 class="mainSection-title">',
+					'after_title'   => '</h2>',
+				)
+			);
 		}
 
-		static function add_widget_area()
-		{
+		static function add_widget_area() {
 			if (
 				is_active_sidebar( 'home-content-top-after-left-widget-area' ) ||
 				is_active_sidebar( 'home-content-top-after-right-widget-area' )
@@ -170,15 +174,15 @@ if ( ! class_exists( 'Lightning_media_posts' ) )
 				echo '<div class="row">';
 				if ( is_active_sidebar( 'home-content-top-after-left-widget-area' ) ) :
 					echo '<div class="col-sm-6">';
-			        dynamic_sidebar( 'home-content-top-after-left-widget-area' );
-			        echo '</div>';
-			    endif;
+					dynamic_sidebar( 'home-content-top-after-left-widget-area' );
+					echo '</div>';
+				endif;
 				if ( is_active_sidebar( 'home-content-top-after-right-widget-area' ) ) :
 					echo '<div class="col-sm-6">';
-			        dynamic_sidebar( 'home-content-top-after-right-widget-area' );
-			        echo '</div>';
-			    endif;
-			    echo '</div>';
+					dynamic_sidebar( 'home-content-top-after-right-widget-area' );
+					echo '</div>';
+				endif;
+				echo '</div>';
 			endif;
 			if ( is_active_sidebar( 'home-content-top-bottom-widget-area' ) ) :
 				echo '<div class="row">';
@@ -193,31 +197,30 @@ if ( ! class_exists( 'Lightning_media_posts' ) )
 		/*  Add media unit css
 		/*-------------------------------------------*/
 
-		static function print_css()
-		{
+		static function print_css() {
 			// デフォルトでは出力しない
 			$print_css_default = false;
-			if( apply_filters('lightning_print_media_posts_css_custom', $print_css_default ) ){
-				wp_enqueue_style( 'lightning_media_posts_style', LTG_MEDIA_POSTS_URL.'css/media_posts.css', array(), LTG_MEDIA_POSTS_VERSION, 'all' );
+			if ( apply_filters( 'lightning_print_media_posts_css_custom', $print_css_default ) ) {
+				wp_enqueue_style( 'lightning_media_posts_style', LTG_MEDIA_POSTS_URL . 'css/media_posts.css', array(), LTG_MEDIA_POSTS_VERSION, 'all' );
 			}
 		}
 
-	    /*-------------------------------------------*/
-	    /*  実行
-	    /*-------------------------------------------*/
+		/*-------------------------------------------*/
+		/*  実行
+		/*-------------------------------------------*/
 
-	    public function __construct(){
+		public function __construct() {
 
-	    	add_action( 'wp_enqueue_scripts',  array( $this, 'print_css' ) );
-	    	add_action( 'lightning_home_content_top_widget_area_after', array( $this, 'add_widget_area' ) );
-	    	add_action( 'widgets_init', array( $this, 'widgets_init' ), 100 );
-	    	// 0.618 = 1:1.618 = 0.38 : 0.62
-			add_image_size('media_thumbnail', 600, 371 , true );
+			add_action( 'wp_enqueue_scripts', array( $this, 'print_css' ) );
+			add_action( 'lightning_home_content_top_widget_area_after', array( $this, 'add_widget_area' ) );
+			add_action( 'widgets_init', array( $this, 'widgets_init' ), 100 );
+			// 0.618 = 1:1.618 = 0.38 : 0.62
+			add_image_size( 'media_thumbnail', 600, 371, true );
 			require_once( 'class.widget.media-posts.php' );
 			require_once( 'views/class.loop-post-view.php' );
 			require_once( 'class.font-awesome-selector.php' );
 			require_once( 'class.media-unit-admin.php' );
-	    }
+		}
 
 	} // class Lightning_media_posts
 
@@ -228,22 +231,21 @@ if ( ! class_exists( 'Lightning_media_posts' ) )
 	/*  Archive Loop change
 	/*-------------------------------------------*/
 	add_filter( 'is_lightning_extend_loop', 'lmu_is_loop_layout_change' );
-	function lmu_is_loop_layout_change( $flag ){
+	function lmu_is_loop_layout_change( $flag ) {
 		$ltg_media_unit_archive_loop_layout = get_option( 'ltg_media_unit_archive_loop_layout' );
-		$post_type = lightning_get_post_type();
-		$postType = $post_type['slug'];
+		$post_type                          = lightning_get_post_type();
+		$postType                           = $post_type['slug'];
 
-		if ( is_author() ){
+		if ( is_author() ) {
 				$postType = 'author';
 		}
 
-		if ( isset ( $ltg_media_unit_archive_loop_layout[$postType] ) ) {
+		if ( isset( $ltg_media_unit_archive_loop_layout[ $postType ] ) ) {
 
 			if (
-				( $ltg_media_unit_archive_loop_layout[$postType] != 'default' ) ||
-				( !$ltg_media_unit_archive_loop_layout[$postType] )
-				)
-			{
+				( $ltg_media_unit_archive_loop_layout[ $postType ] != 'default' ) ||
+				( ! $ltg_media_unit_archive_loop_layout[ $postType ] )
+				) {
 					add_action( 'lightning_extend_loop', 'lmu_do_loop_layout_change' );
 					$flag = true;
 			}
@@ -251,21 +253,20 @@ if ( ! class_exists( 'Lightning_media_posts' ) )
 		return $flag;
 	}
 
-	function lmu_do_loop_layout_change(){
+	function lmu_do_loop_layout_change() {
 		$ltg_media_unit_archive_loop_layout = get_option( 'ltg_media_unit_archive_loop_layout' );
-		$post_type = lightning_get_post_type();
-		$post_type_slug = $post_type['slug'];
-		$post_type_slug = ( is_author() ) ? 'author' : $post_type['slug'];
-		if ( isset( $ltg_media_unit_archive_loop_layout[$post_type_slug] ) ){
+		$post_type                          = lightning_get_post_type();
+		$post_type_slug                     = $post_type['slug'];
+		$post_type_slug                     = ( is_author() ) ? 'author' : $post_type['slug'];
+		if ( isset( $ltg_media_unit_archive_loop_layout[ $post_type_slug ] ) ) {
 
-			$layout = $ltg_media_unit_archive_loop_layout[$post_type_slug];
+			$layout                       = $ltg_media_unit_archive_loop_layout[ $post_type_slug ];
 			$instance['new_icon_display'] = 7;
 			echo '<div class="loop_outer">';
 
-			Ltg_Media_Post_View::post_loop($layout, $instance);
+			Ltg_Media_Post_View::post_loop( $layout, $instance );
 
 			echo '</div>';
 		}
 	}
-
 } // if ( ! class_exists( 'Lightning_media_posts' ) )
