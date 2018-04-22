3d484b37567b24c7cbd618866615f9a124e2a928
kurudrive <kurudrive@gmail.com>
2018-04-23 01:33:57 +0900

    [ vk-prlx ] 背景位置計算間違い修正 / 軽量化の為に変数名を短縮

--- js-libraries/vk-prlx.js
+++ js-libraries/vk-prlx.js
@@ -25,7 +25,7 @@
 	function prlx_scr() {
 
 		// スクロールした量
-		var scrolled_Y = jQuery(window).scrollTop();
+		var scrled_Y = jQuery(window).scrollTop();
 		// ウィンドウの高さ
 		var window_H = document.documentElement.clientHeight;
 		// 画像URLを取得する正規表現
@@ -63,13 +63,13 @@
 			// 1...スクロールした分だけ画像が先に上がる（表示されている背景画像の下方向が見えるようになっていく）
 			// 0...スクロールと同時に画像が動く（パララックス無しと同じ）
 			// -1 ... スクロールした分だけ画像が遅れて上がる（表示されている背景画像の上方向が見えるようになっていく）
-			var scroll_rate = -1;
+			var scrl_rate = -1;
 
-			if (0 < scroll_rate) {
+			if (0 < scrl_rate) {
 				// 最低限必要な画像の高さ = エリアの高さ + ( エリアの高さ * 動かす比率 )
-				var need_H = area_H + (area_H * scroll_rate);
+				var need_H = area_H + (area_H * scrl_rate);
 			} else {
-				var need_H = area_H + (area_H * -scroll_rate);
+				var need_H = area_H + (area_H * -scrl_rate);
 			}
 
 			// もし 必要な高さがもともとある場合
@@ -98,10 +98,10 @@
 			/*-------------------------------------*/
 
 			// 表示領域最上部から対象の最上部までの高さ
-			var position_Y = jQuery(this).offset().top;
+			var posi_Y = jQuery(this).offset().top;
 
 			// 開始位置 = パララックスエリアの位置 - ウィンドウの高さ
-			var prlx_start_Y = position_Y - window_H;
+			var prlx_start_Y = posi_Y - window_H;
 			// はみ出してる高さ = 画像の高さ - 表示エリアの高さ
 			var bg_over_H = bg_H - area_H;
 			// 背景を動かす間の距離 = ウィンドウサイズ + パララックスエリアの高さ
@@ -114,12 +114,12 @@
 			var bg_offset_distance = (need_H - area_H) / prlx_area_H;
 
 			// パララックス開始
-			if (prlx_start_Y < scrolled_Y) {
+			if (prlx_start_Y < scrled_Y) {
 
 				// 開始位置からスクロールした量
-				var prlx_scrolled_Y = (scrolled_Y - prlx_start_Y);
+				var prlx_scrled_Y = (scrled_Y - prlx_start_Y);
 				// 背景画像を移動させる距離
-				var bg_position_Y = prlx_scrolled_Y * bg_offset_distance;
+				var bg_posi_Y = prlx_scrled_Y * bg_offset_distance;
 
 				// 最低限必要な画像高さから背景画像の高さがはみ出している場合
 				if (need_H < bg_H) {
@@ -130,24 +130,24 @@
 				}
 
 				// 背景画像が先に上がる場合（背景画像の下方向が見えるようになる）
-				if (0 < scroll_rate) {
+				if (0 < scrl_rate) {
 					// 背景画像は表示エリアと上端が揃った状態でスタートし、上方向に移動するので - にする
-					bg_position_Y = -bg_position_Y;
+					bg_posi_Y = -bg_posi_Y;
 					// 画像がはみ出している分を更に上方向に補正
-					bg_position_Y = bg_position_Y - over_offset_H;
+					bg_posi_Y = bg_posi_Y - over_offset_H;
 
 					// 背景画像が遅れて上がる場合（背景画像の上方向が見えるようになる）
-				} else if (0 > scroll_rate) {
+				} else if (0 > scrl_rate) {
 					// 上方向が見えるようにするために予めマイナスオフセットして、スクロールする事でマイナスが減っていく
 
 					// 遅れて上がる場合のマイナスオフセット値 = 必要高さ - 表示エリア
 					var bottom_offset = -(need_H - area_H);
 					// マイナスオフセットしてあった分からスクロールした分だけマイナスオフセットが減っていく（上が見えるようになる）
-					bg_position_Y = bottom_offset + bg_position_Y;
-					// 画像がはみ出している分を更に下方向に補正
+					bg_posi_Y = bottom_offset + bg_posi_Y;
+					// 画像がはみ出している分を更に上方向に補正
 					// ※必要高さよりも背景画像が高い場合は下端がはみ出した状態でスタートさせる形になる
-					//  → 背景画像の位置を下げるために オフセット量がプラス方向に増える
-					bg_position_Y = bg_position_Y + over_offset_H;
+					//  → 背景画像が必要高さからはみ出しているので、その分上にオフセットするためマイナス方向に増える
+					bg_posi_Y = bg_posi_Y - over_offset_H;
 
 				}
 
@@ -159,10 +159,10 @@
 				// console.log('[' + i + '] 1スクロールで動かす背景の距離 : ' + bg_offset_distance);
 				// console.log('[' + i + '] 必要高さからはみ出している画像の高さ : ' + bg_need_over_H);
 				// console.log('[' + i + '] はみ出しているのでオフセットする高さ : ' + over_offset_H);
-				// console.log('[' + i + '] Y : ' + bg_position_Y);
+				// console.log('[' + i + '] Y : ' + bg_posi_Y);
 
 				// 背景の位置を指定
-				var backgroundPosition = "center " + bg_position_Y + "px";
+				var backgroundPosition = "center " + bg_posi_Y + "px";
 
 			}
 			// console.log('area_W : ' + area_W + ' / area_H : ' + area_H);
