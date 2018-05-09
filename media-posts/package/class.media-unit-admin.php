80a81b441a606f94386ec6f12878011360aca9b5
kurudrive <kurudrive@gmail.com>
2018-05-09 18:33:23 +0900

    [ media posts ] phpcs

--- media-posts/package/class.media-unit-admin.php
+++ media-posts/package/class.media-unit-admin.php
@@ -3,13 +3,13 @@
 class Lightning_Media_Admin {
 
 	//controls post types that can be displayed with grid layout on front-end (archives list pages)
-	private static $post_types = array( 'post' => 0 );
+	private static $post_types        = array( 'post' => 0 );
 	private static $post_types_labels = array();
 
 	public static function init() {
 
 		// Add a link to this plugin's settings page
-        // add_filter('plugin_action_links_'.LTG_MEDIA_BASENAME , array( __CLASS__, 'set_plugin_meta'), 10, 1);
+		// add_filter('plugin_action_links_'.LTG_MEDIA_BASENAME , array( __CLASS__, 'set_plugin_meta'), 10, 1);
 
 		//gets custom post types too
 		self::$post_types = Lightning_media_posts::get_custom_types() + self::$post_types;
@@ -26,29 +26,28 @@ class Lightning_Media_Admin {
 
 		$mess = self::validSettings();
 
-		$post_types_label 		= self::$post_types_labels;
-		$post_types 			= self::$post_types;
+		$post_types_label = self::$post_types_labels;
+		$post_types       = self::$post_types;
 
-		require_once( 'views/setting-page.php');
+		require_once( 'views/setting-page.php' );
 	}
 
 	//valid admin form and save post types selected by user in DB
-	private static function validSettings()
-	{
+	private static function validSettings() {
 		global $vk_ltg_media_posts_textdomain;
 
 		$mess = '';
 
 		//valid form
-		if ( isset($_POST) && !empty($_POST)
-			&& isset($_POST['lightning-media-unit-nonce']) && wp_verify_nonce( $_POST['lightning-media-unit-nonce'], 'lightning-media-unit-submit' ) ) {
+		if ( isset( $_POST ) && ! empty( $_POST )
+			&& isset( $_POST['lightning-media-unit-nonce'] ) && wp_verify_nonce( $_POST['lightning-media-unit-nonce'], 'lightning-media-unit-submit' ) ) {
 
-			$post_types = Lightning_media_posts::get_custom_types() + self::$post_types;
+			$post_types           = Lightning_media_posts::get_custom_types() + self::$post_types;
 			$post_types['author'] = 0;
 
 			foreach ( $post_types as $type => $value ) {
-				if( isset( $_POST['ltg_media_unit_archive_loop_layout'][$type] ) ) {
-					$ltg_media_unit_archive_loop_layout[ $type ] = esc_html(  $_POST['ltg_media_unit_archive_loop_layout'][$type] );
+				if ( isset( $_POST['ltg_media_unit_archive_loop_layout'][ $type ] ) ) {
+					$ltg_media_unit_archive_loop_layout[ $type ] = esc_html( $_POST['ltg_media_unit_archive_loop_layout'][ $type ] );
 				}
 			}
 
@@ -60,43 +59,48 @@ class Lightning_Media_Admin {
 		return $mess;
 	}
 
-	public static function archive_layout_customize_register( $wp_customize )
-	{
+	public static function archive_layout_customize_register( $wp_customize ) {
 
 		global $vk_ltg_media_posts_textdomain;
-		$wp_customize->add_section( 'lightning_archive_layout', array(
-			'title'				=> __('Lightning Archive Layouts', $vk_ltg_media_posts_textdomain ),
-			'priority'			=> 800,
-		) );
+		$wp_customize->add_section(
+			'lightning_archive_layout', array(
+				'title'    => __( 'Lightning Archive Layouts', $vk_ltg_media_posts_textdomain ),
+				'priority' => 800,
+			)
+		);
 
-		$post_types = array( 'post' => 0 );
-		$post_types = Lightning_media_posts::get_custom_types() + $post_types;
+		$post_types           = array( 'post' => 0 );
+		$post_types           = Lightning_media_posts::get_custom_types() + $post_types;
 		$post_types['author'] = 'author';
 
-		$post_types_labels = Lightning_media_posts::labelNames() + Lightning_media_posts::get_custom_types_labels();
+		$post_types_labels           = Lightning_media_posts::labelNames() + Lightning_media_posts::get_custom_types_labels();
 		$post_types_labels['author'] = __( 'Author', $vk_ltg_media_posts_textdomain );
 
 		$patterns['default']['label'] = __( 'Lightning default', $vk_ltg_media_posts_textdomain );
-		$patterns = $patterns + Lightning_media_posts::patterns();
-		foreach ($patterns as $key => $value) {
-			$layouts[$key] = $value['label'];
+		$patterns                     = $patterns + Lightning_media_posts::patterns();
+		foreach ( $patterns as $key => $value ) {
+			$layouts[ $key ] = $value['label'];
 		}
 
 		foreach ( $post_types as $type => $value ) {
-			$wp_customize->add_setting( 'ltg_media_unit_archive_loop_layout['.$type.']', array(
-				'default'			=> 'default',
-				'type'				=> 'option',
-				'capability'		=> 'edit_theme_options',
-				'sanitize_callback' => 'sanitize_text_field',
-			) );
-			$wp_customize->add_control( 'ltg_media_unit_archive_loop_layout['.$type.']', array(
-				'label'		=>  sprintf( __( 'Archive Page Layout [ %s ]', $vk_ltg_media_posts_textdomain ),$post_types_labels[$type] ),
-				'section'	=> 'lightning_archive_layout',
-				'settings'  => 'ltg_media_unit_archive_loop_layout['.$type.']',
-				'type'		=> 'select',
-				'choices'   => $layouts,
-				'priority'	=> 500,
-			));
+			$wp_customize->add_setting(
+				'ltg_media_unit_archive_loop_layout[' . $type . ']', array(
+					'default'           => 'default',
+					'type'              => 'option',
+					'capability'        => 'edit_theme_options',
+					'sanitize_callback' => 'sanitize_text_field',
+				)
+			);
+			$wp_customize->add_control(
+				'ltg_media_unit_archive_loop_layout[' . $type . ']', array(
+					'label'    => sprintf( __( 'Archive Page Layout [ %s ]', $vk_ltg_media_posts_textdomain ), $post_types_labels[ $type ] ),
+					'section'  => 'lightning_archive_layout',
+					'settings' => 'ltg_media_unit_archive_loop_layout[' . $type . ']',
+					'type'     => 'select',
+					'choices'  => $layouts,
+					'priority' => 500,
+				)
+			);
 		}
 
 		return $wp_customize;
