6e0d1ef1357ca1c440b752c0280912438af58fd0
kurudrive <kurudrive@gmail.com>
2018-05-09 19:25:53 +0900

    [ media posts ] PHPCS

--- media-posts/package/views/setting-page.php
+++ media-posts/package/views/setting-page.php
@@ -1,5 +1,5 @@
 <div class="fileedit-sub"></div>
-<?php 
+<?php
 echo $mess;
 global $vk_ltg_media_posts_textdomain; ?>
 <div id="welcome-panel" class="message_intro">
@@ -7,27 +7,38 @@ global $vk_ltg_media_posts_textdomain; ?>
 </div>
 
 <form action="<?php echo $_SERVER['REQUEST_URI']; ?>" method="post">
-	<?php wp_nonce_field( 'lightning-media-unit-submit', 'lightning-media-unit-nonce'); ?>
+	<?php wp_nonce_field( 'lightning-media-unit-submit', 'lightning-media-unit-nonce' ); ?>
 	<div class="sectionBox">
 		<table>
 			<?php
-			$post_types = array( 'post' => 0 );
-			$post_types = Lightning_media_posts::get_custom_types() + $post_types;
-			$post_types_labels = Lightning_media_posts::labelNames() + Lightning_media_posts::get_custom_types_labels();
-			$ltg_media_unit_archive_loop_layout = get_option('ltg_media_unit_archive_loop_layout');
-			foreach ( $post_types as $type => $value ) {
-				$selected = isset( $ltg_media_unit_archive_loop_layout[$type] ) ? $ltg_media_unit_archive_loop_layout[$type] : 'default';
-				echo '<tr><th class="text-left">'.$post_types_labels[$type].'</th><td>';
-				echo '<select name="ltg_media_unit_archive_loop_layout['.$type.']" id="ltg_media_unit_archive_loop_layout_'.$type.'">';
-				Lightning_media_posts::patterns_select_options( $selected );
-				echo '</select></td></tr>'."\n";
+			$post_types                         = array( 'post' => 0 );
+			$post_types                         = Lightning_media_posts::get_custom_types() + $post_types;
+			$post_types_labels                  = Lightning_media_posts::labelNames() + Lightning_media_posts::get_custom_types_labels();
+			$ltg_media_unit_archive_loop_layout = get_option( 'ltg_media_unit_archive_loop_layout' );
+
+			// Works Unit のカスタム投稿タイプがある場合は除外する
+			include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+			if ( is_plugin_active( 'lightning-works-unit/lightning-works-unit.php' ) ) {
+				$works_unit = get_option( 'lightning_works_unit' );
+				if ( ! empty( $works_unit['post_name'] ) ) {
+					$works_unit_slug = $works_unit['post_name'];
+					unset( $post_types[ $works_unit_slug ] );
+				}
 			}
 
-			echo '<tr><th>'.__( 'Author Archive', $vk_ltg_media_posts_textdomain ).'</th><td>';
+			foreach ( $post_types as $type => $value ) {
+				$selected = isset( $ltg_media_unit_archive_loop_layout[ $type ] ) ? $ltg_media_unit_archive_loop_layout[ $type ] : 'default';
+				echo '<tr><th class="text-left">' . $post_types_labels[ $type ] . '</th><td>';
+				echo '<select name="ltg_media_unit_archive_loop_layout[' . $type . ']" id="ltg_media_unit_archive_loop_layout_' . $type . '">';
+				Lightning_media_posts::patterns_select_options( $selected );
+				echo '</select></td></tr>' . "\n";
+			}
+
+			echo '<tr><th>' . __( 'Author Archive', $vk_ltg_media_posts_textdomain ) . '</th><td>';
 			echo '<select name="ltg_media_unit_archive_loop_layout[author]" id="ltg_media_unit_archive_loop_layout_author">';
 			$selected = isset( $ltg_media_unit_archive_loop_layout['author'] ) ? $ltg_media_unit_archive_loop_layout['author'] : 'default';
 			Lightning_media_posts::patterns_select_options( $selected );
-			echo '</select></td></tr>'."\n";
+			echo '</select></td></tr>' . "\n";
 
 			?>
 		</table>
