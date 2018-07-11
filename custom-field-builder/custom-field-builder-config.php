fd35865e8d2c9de2f2ac27a42e792beaba486766
naoki <n.globe.us@gmail.com>
2018-07-11 15:59:48 +0900

    admin画面で、datepicker.jsとmediauploader.jsとcf-builder.cssが読み込めない問題解決

--- custom-field-builder/custom-field-builder-config.php
+++ custom-field-builder/custom-field-builder-config.php
@@ -3,10 +3,15 @@
 /*-------------------------------------------*/
 /*  Load modules
 /*-------------------------------------------*/
+
 if ( ! class_exists( 'VK_Custom_Field_Builder' ) )
 {
 	require_once( 'custom-field-builder/custom-field-builder.php' );
 
 	global $custom_field_builder_url;
-	$custom_field_builder_url = LIGHTNING_SKIN_JPNSTYLE_URL.'inc/custom-field-builder/';
+	if(defined('LIGHTNING_SKIN_JPNSTYLE_URL')){
+		$custom_field_builder_url = LIGHTNING_SKIN_JPNSTYLE_URL.'inc/custom-field-builder/';
+	}elseif (defined('LIGHTNING_PRO_THEME_URL')){
+		$custom_field_builder_url = LIGHTNING_PRO_THEME_URL.'inc/custom-field-builder/';
+	};
 }
