c01d0cf04730167621559bd83691af6719ba1b48
kurudrive <kurudrive@gmail.com>
2019-05-27 20:37:51 +0900

    [ media posts ] 直 echo を変更

--- media-posts/package/views/class-loop-post-view.php
+++ media-posts/package/views/class-loop-post-view.php
@@ -3,6 +3,13 @@ if ( ! class_exists( 'Ltg_Media_Post_View' ) ) {
 
 	class Ltg_Media_Post_View {
 
+		/**
+		 * [post_loop description]
+		 *
+		 * @param  [type] $layout   [description]
+		 * @param  [type] $instance [description]
+		 * @return [type]           [description]
+		 */
 		public static function post_loop( $layout, $instance ) {
 			$patterns = Lightning_media_posts::patterns();
 			echo '<div class="' . $patterns[ $layout ]['class_outer'] . '">';
@@ -48,11 +55,12 @@ if ( ! class_exists( 'Ltg_Media_Post_View' ) ) {
 			echo '</div>';
 		}
 
-		public static function media_post_item( $media_post_class, $instance = '' ) {
+		public static function get_media_post_item( $media_post_class, $instance = '' ) {
 			global $post;
-			echo '<article class="media_post media_post_base' . $media_post_class . '" id="post-' . get_the_ID() . '">' . "\n";
-			echo '<a href="' . esc_url( get_the_permalink() ) . '">' . "\n";
-			$thumbnail_id = get_post_thumbnail_id( $post->ID );
+			$media_post_item  = '';
+			$media_post_item .= '<article class="media_post media_post_base' . $media_post_class . '" id="post-' . get_the_ID() . '">' . "\n";
+			$media_post_item .= '<a href="' . esc_url( get_the_permalink() ) . '">' . "\n";
+			$thumbnail_id     = get_post_thumbnail_id( $post->ID );
 			if ( $thumbnail_id ) {
 				$thumbnail_src  = wp_get_attachment_image_src( $thumbnail_id, 'large' );
 				$thumbnail_src  = $thumbnail_src[0];
@@ -61,41 +69,45 @@ if ( ! class_exists( 'Ltg_Media_Post_View' ) ) {
 				$thumbnail_src  = VK_MEDIA_POSTS_URL . 'images/no-image.png';
 				$class_no_image = ' noimage';
 			}
-			echo '<div class="media_post_image' . $class_no_image . '" style="background-image:url(' . $thumbnail_src . ');">' . "\n";
-			$thumbnail = get_the_post_thumbnail( $post->ID, 'media_thumbnail' );
-			echo  ( $thumbnail ) ? $thumbnail : '<img src="' . $thumbnail_src . '" alt="NO IMAGE" />';
-			echo '</div>';
+			$media_post_item .= '<div class="media_post_image' . $class_no_image . '" style="background-image:url(' . $thumbnail_src . ');">' . "\n";
+			$thumbnail        = get_the_post_thumbnail( $post->ID, 'media_thumbnail' );
+			$media_post_item .= ( $thumbnail ) ? $thumbnail : '<img src="' . $thumbnail_src . '" alt="NO IMAGE" />';
+			$media_post_item .= '</div>';
 			// ※アーカイブページの場合はこのメソッドが呼び出される時点で instance に数字が入っているで、ここの数字を変更しても反映されない
 			$days  = isset( $instance['new_icon_display'] ) ? $instance['new_icon_display'] : 7; // Newを表示させたい期間の日数
 			$today = date_i18n( 'U' );
 			$entry = get_the_time( 'U' );
 			$kiji  = date( 'U', ( $today - $entry ) ) / 86400;
 			if ( $days > $kiji ) {
-				echo '<div class="media_post_label_new">NEW</div>';
+				$media_post_item .= '<div class="media_post_label_new">NEW</div>';
 			}
 
-			echo '<div class="media_post_text">' . "\n";
-			echo '<div class="media_post_meta">' . "\n";
-			echo '<span class="published media_post_date">' . esc_html( get_the_date() ) . '</span>';
-			$taxonomies = get_the_taxonomies();
+			$media_post_item .= '<div class="media_post_text">' . "\n";
+			$media_post_item .= '<div class="media_post_meta">' . "\n";
+			$media_post_item .= '<span class="published media_post_date">' . esc_html( get_the_date() ) . '</span>';
+			$taxonomies       = get_the_taxonomies();
 			if ( $taxonomies ) :
 				// get $taxonomy name
-				$taxonomy   = key( $taxonomies );
-				$terms      = get_the_terms( get_the_ID(), $taxonomy );
-				$term_name  = esc_html( $terms[0]->name );
-				$term_color = Vk_term_color::get_term_color( $terms[0]->term_id );
-				$term_color = ( $term_color ) ? ' style="background-color:' . $term_color . '"' : '';
-				echo '<span class="media_post_term"' . $term_color . '>' . $term_name . '</span>';
+				$taxonomy         = key( $taxonomies );
+				$terms            = get_the_terms( get_the_ID(), $taxonomy );
+				$term_name        = esc_html( $terms[0]->name );
+				$term_color       = Vk_term_color::get_term_color( $terms[0]->term_id );
+				$term_color       = ( $term_color ) ? ' style="background-color:' . $term_color . '"' : '';
+				$media_post_item .= '<span class="media_post_term"' . $term_color . '>' . $term_name . '</span>';
 			endif;
-			echo '<span class="vcard author"><span class="fn">' . get_the_author() . '</span></span>';
-			echo '</div>' . "\n"; // entry-meta
-			echo '<h4 class="media_post_title">' . esc_html( get_the_title() ) . '</h4>';
-			echo '<p class="media_post_excerpt">' . esc_html( get_the_excerpt() ) . '</p>';
-			echo '</div>';
-			echo '</a>';
-			echo '</article>';
+			$media_post_item .= '<span class="vcard author"><span class="fn">' . get_the_author() . '</span></span>';
+			$media_post_item .= '</div>' . "\n"; // entry-meta
+			$media_post_item .= '<h4 class="media_post_title">' . esc_html( get_the_title() ) . '</h4>';
+			$media_post_item .= '<p class="media_post_excerpt">' . esc_html( get_the_excerpt() ) . '</p>';
+			$media_post_item .= '</div>';
+			$media_post_item .= '</a>';
+			$media_post_item .= '</article>';
 		}
 
-	}
+		public static function media_post_item( $media_post_class, $instance = '' ) {
+			echo Ltg_Media_Post_View::get_media_post_item( $media_post_class, $instance = '' );
+		}
 
-}
+	} // class Ltg_Media_Post_View {
+
+} // if ( ! class_exists( 'Ltg_Media_Post_View' ) ) {
