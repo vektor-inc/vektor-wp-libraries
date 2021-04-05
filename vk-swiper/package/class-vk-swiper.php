0b9b9de54903a37f7cf6e52ea9681ab37a52d5eb
drill-lancer <rickaddison7634@gmail.com>
2021-04-05 12:09:23 +0900

    fix swiper

--- vk-swiper/package/class-vk-swiper.php
+++ vk-swiper/package/class-vk-swiper.php
@@ -26,18 +26,29 @@ if ( ! class_exists( 'VK_Swiper' ) ) {
 		/**
 		 * Init
 		 */
-		public static function init() {
+		public function __construct() {
 			add_action( 'wp_enqueue_scripts', array( __CLASS__, 'load_swiper' ) );
+			add_filter( 'vk_css_tree_shaking_array', array( __CLASS__, 'css_tree_shaking_array' ), 11 );
 		}
 
 		/**
 		 * Load Swiper
 		 */
 		public static function load_swiper() {
-			global $vk_swiper_url;
-			wp_enqueue_style( 'vk-swiper-style', $vk_swiper_url . '/assets/css/swiper.min.css', array(), SWIPER_VERSION );
-			wp_enqueue_script( 'vk-swiper-script', $vk_swiper_url . '/assets/js/swiper.min.js', array(), SWIPER_VERSION, true );
+			wp_enqueue_style( 'vk-swiper-style', plugin_dir_url( __FILE__ ) . 'assets/css/swiper.min.css', array(), SWIPER_VERSION );
+			wp_enqueue_script( 'vk-swiper-script', plugin_dir_url( __FILE__ ) . 'assets/js/swiper.min.js', array(), SWIPER_VERSION, true );
+		}
+
+		public static function css_tree_shaking_array( $vk_css_tree_shaking_array ){
+			$vk_css_tree_shaking_array[] = array(
+				'id'      => 'vk-swiper-style',
+				'url'     => plugin_dir_url( __FILE__ ) . 'assets/css/swiper.min.css',
+				'path'    => plugin_dir_path( __FILE__ ) . 'assets/css/swiper.min.css',
+				'version' => SWIPER_VERSION,
+			);
+			return $vk_css_tree_shaking_array;
 		}
 	}
-	VK_Swiper::init();
+	new VK_Swiper();
 }
+
