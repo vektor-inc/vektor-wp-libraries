ef251e8168258d7fde0276df7e29843342dc092f
drill-lancer <rickaddison7634@gmail.com>
2021-04-23 07:01:27 +0900

    update filter search

--- filter-search/package/build/block.asset.php
+++ filter-search/package/build/block.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-polyfill', 'wp-server-side-render'), 'version' => 'dd6613c69734fda455b09754f2b70877');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-polyfill', 'wp-server-side-render'), 'version' => 'aa88ea2e1d33d57c657186fd898ae926');
