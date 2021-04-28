2e2ac189b10b70a5d08f3c7b5154c2b69a80f7ce
drill-lancer <rickaddison7634@gmail.com>
2021-04-28 09:51:57 +0900

    add vk-helpers to library

---
+++ vk-helpers/config.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * VK Helpers Config
+ */
+
+if ( ! class_exists( 'VK_Helpers' ) ) {
+	require_once dirname( __FILE__ ) . '/package/class-vk-helpers.php';
+}
+
+if ( ! class_exists( 'VK_Custom_Html_Control' ) ) {
+	require_once dirname( __FILE__ ) . '/package/class-vk-custom-html-control.php';
+}
+
+if ( ! class_exists( 'VK_Custom_Text_Control' ) ) {
+	require_once dirname( __FILE__ ) . '/package/class-vk-custom-text-control.php';
+}
+
