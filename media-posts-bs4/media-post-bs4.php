d2127f27af3df124b97ff9abedc8f1612af60937
drill-lancer <rickaddison7634@gmail.com>
2021-06-16 14:08:14 +0900

    feature: 翻訳ファイルを packages 内に独自に所持するように変更

---
+++ media-posts-bs4/media-post-bs4.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: VK Media Posts BS4
+ * Plugin URI: https://github.com/vektor-inc/vektor-wp-libraries/
+ * Description: This is a plugin that you can add a filter search function with blocks.
+ * Version: 1.3.0
+ * Requires at least: 5.7
+ * Author:  Vektor,Inc.
+ * Author URI: https://vektor-inc.co.jp
+ * Text Domain: vk-media-posts-bs4
+ * Domain Path: /languages
+ * License: GPL 2.0 or Later
+ *
+ * @package VK Media Posts BS4
+ */
