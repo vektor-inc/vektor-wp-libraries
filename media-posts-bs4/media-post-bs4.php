28546edd0d923882dca2cd664001e38e93d6ffd7
drill-lancer <rickaddison7634@gmail.com>
2021-06-16 14:46:15 +0900

    fix: a file

--- media-posts-bs4/media-post-bs4.php
+++ media-posts-bs4/media-post-bs4.php
@@ -1,12 +1,12 @@
 <?php
 /**
  * Plugin Name: VK Media Posts BS4
- * Plugin URI: 
- * Description: 
+ * Plugin URI:
+ * Description:
  * Version: 1.3.0
  * Requires at least: 5.7
  * Author:
- * Author URI: 
+ * Author URI:
  * Text Domain: vk-media-posts-bs4
  * Domain Path: package/languages
  * License: GPL 2.0 or Later
