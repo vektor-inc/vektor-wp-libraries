0a3c22c42d20e0125aa036a45ff68fa2bc9762da
kurudrive <kurudrive@gmail.com>
2022-07-06 23:03:22 +0900

    [ CF Builder ] jQuery UI の CSS が意図せず適用される不具合を修正（除外フック追加）

--- custom-field-builder/package/custom-field-builder.php
+++ custom-field-builder/package/custom-field-builder.php
@@ -56,7 +56,7 @@ if ( ! class_exists( 'VK_Custom_Field_Builder' ) ) {
 			wp_enqueue_style( 'cf-builder-style', self::admin_directory_url() . 'css/cf-builder.css', array(), self::$version, 'all' );
 
 			// Contact form 7　など jQuery ui のクラス名を使っていて干渉するので除外 .
-			$cfb_jquery_ui_excludes =  array( 'toplevel_page_wpcf7', 'toplevel_page_gf_edit_forms' );
+			$cfb_jquery_ui_excludes = array( 'toplevel_page_wpcf7', 'toplevel_page_gf_edit_forms' );
 			$cfb_jquery_ui_excludes = apply_filters( 'cfb_jquery_ui_excludes', $cfb_jquery_ui_excludes );
 			if ( ! in_array( $hook_suffix, $cfb_jquery_ui_excludes ) ) {
 				wp_enqueue_style( 'cf-builder-jquery-ui-style', self::admin_directory_url() . 'css/jquery-ui.css', array( 'cf-builder-style' ), self::$version, 'all' );
